f7b40f1fdee42efbc53e22ece5ce5b7dc60c1a20
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2024-03-11 02:39:02 +0200

    add js for "features" section

--- js/script-opendeskr.js
+++ js/script-opendeskr.js
@@ -1,12 +1,12 @@
 const featuresList = document.querySelectorAll(".features-goals-list-item");
-const featuresText = document.querySelectorAll(".features-overlay-text");
 
 featuresList.forEach((elem) => {
-  elem.classList.add("increase");
-  elem.addEventListener("click", function () {
-    featuresText.forEach((elem) => {
-      elem.innerHTML = `You have 30 minutes to find a relic, signal for extraction, and grab
-one of three spots on the rescue chopper.`;
-    });
+  elem.addEventListener("click", (event) => {
+    let featuresMission = `<p class="increase">You have 30 minutes to find a relic, signal for extraction, and grab one of three spots on the rescue chopper.</p>`;
+    if (event.target.querySelector(".increase")) {
+      event.target.querySelector(".increase").remove();
+    } else {
+      event.target.insertAdjacentHTML("beforeend", featuresMission);
+    }
   });
 });
